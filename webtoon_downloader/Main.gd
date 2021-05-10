f3cc44176121bbe16b43659c7e4508b9877c0147
Eunhyeok Kwon <eunhyeoq@gmail.com>
2021-05-11 00:04:48 +0900

    Add single episode download function

--- webtoon_downloader/Main.gd
+++ webtoon_downloader/Main.gd
@@ -1,20 +1,43 @@
 extends Node2D
 
-var img_data:Array = []
+
+# global variables to store shared data
+var img_urls:Array = []
 var img_save_path:String
-var base_url = "https://comic.naver.com/webtoon/detail.nhn?titleId=%d&no=%d"
+var request_header
+var current_episode:int # for storing currently downloading episode number
+
+# node shortcuts
+var _output_display
+var _titleid_input
+var _episode_from_input
+var _episode_to_input
+
 
 func _ready():
-	pass
+	_output_display = $Node/OuputDisplay
+	_titleid_input = $Node/TitleIdInput
+	_episode_from_input = $Node/EpisodeFromInput
+	_episode_to_input = $Node/EpisodeToInput
 
 
-func add_text(RTlabel:RichTextLabel, string:String, newline:bool = true):
+func get_titleid_val():
+	return _titleid_input.text.to_int()
+func get_episode_from_val():
+	return _episode_from_input.value
+func get_episode_to_val():
+	return _episode_to_input.value
+
+
+# adds string to OuputDisplay node
+func print_on_output_display(string:String, newline:bool = true):
 	if newline:
-		RTlabel.text += "\n" + string
+		_output_display.text += "\n" + string
 	else:
-		RTlabel.text += string
+		_output_display.text += string
 
 
+# function for saving data on OS file -- for debugging
 func append_to_text_file(file_path:String, string:String):
 	var file = File.new()
 	var error = file.open(file_path, file.READ_WRITE)
@@ -31,17 +54,39 @@ func append_to_text_file(file_path:String, string:String):
 	file.close()
 
 
-func get_requeste_url() -> String:
-	var titleid = $Node/TitleIdInput.text.to_int()
-	var episode_from = $Node/EpisodeFrom.value
-	var episode_to= $Node/EpisodeTo.value
-	var request_url = base_url % [titleid, episode_to]
-	return request_url
-
-func get_naver_comic():
-	$HTTPRequest.request(get_requeste_url())
+# creates a valid url address to specific title and episode of naver comic
+func create_naver_comic_url(titleId, episode) -> String:
+	return "https://comic.naver.com/webtoon/detail.nhn?titleId=%d&no=%d" % [titleId, episode]
 
 
+# ***** 1 *****
+func _on_DownloadButton_pressed():
+	# download single episode page
+	if get_episode_from_val() == get_episode_to_val():
+		$HTTPRequest.connect("request_completed", self, "_on_getWebtoonPage_request_completed")
+		$HTTPRequest.request(create_naver_comic_url(get_titleid_val(), get_episode_to_val()))
+	else:
+		print_on_output_display("downloading multiple episode function is not ready yet!")
+
+
+# ***** 2 *****
+func _on_getWebtoonPage_request_completed(result, response_code, headers, body):
+	print_HTTPRequest_RRH_on_output_display(result, response_code, headers)
+	img_urls.clear()
+	img_urls = parse_img_data_from_html(body)
+	
+	# debug
+	for i in img_urls:
+		append_to_text_file("/tmp/guest-6fr2gu/Desktop/test.txt", i)
+	
+	if img_urls != []:
+		$FileDialog.popup_centered()
+	else:
+		print_on_output_display("No Imgage url was Found...")
+
+
+# ***** 2.5 ***** (used in step 2)
+# extracts img urls from webtoon page
 func parse_img_data_from_html(html_data_buffer:PoolByteArray) -> Array:
 	var img_scr_list = []
 	var xml = XMLParser.new()
@@ -50,12 +95,13 @@ func parse_img_data_from_html(html_data_buffer:PoolByteArray) -> Array:
 		printerr("Error has accured while trying to OPEN xml buffer...  error code:", error)
 		return []
 		
-	error = xml.read()
+	error = xml.read()	# begin reading the first line of xml
 	if error != OK:
 		printerr("Error has accured while trying to READ xml buffer...  error code:", error)
 		return []
 	
-	var temp_path = "C:/Users/admin/Documents/GitHub/test.txt" #for windows computer
+#	var temp_path = "C:/Users/admin/Documents/GitHub/test.txt" # for windows computer debug
+	var temp_path = "/tmp/guest-6fr2gu/Desktop/test.txt" # for linux computer debug
 #	var path= "user://test.txt"
 	
 	while !error:
@@ -75,69 +121,39 @@ func parse_img_data_from_html(html_data_buffer:PoolByteArray) -> Array:
 				pass
 		error = xml.read()
 		if error != OK:
+			# when reached EOF, break loop
 			if error == ERR_FILE_EOF:
 				break
-			printerr("Error has accured while trying to READ xml buffer...  error code:", error)
-			return []
+			else:
+				printerr("Error has accured while trying to READ xml buffer...  error code:", error)
+				return []
+
 	return img_scr_list
 
 
-func download_img_to_local(urls:Array, dir:String):
-	var i = 0
-	var header = ["User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
-				 "Referer: " + get_requeste_url() 
-				]
-	$HTTPRequestImg.request(urls[0], header)
-	
-#	for img_url in urls:
-#		$HTTPRequestImg.request(img_url, header)
-#		img_save_path = dir + str(i) + ".png"
-#		print("worked!", i)
-#		i += 1
-
-
-func _on_HTTPRequest_request_completed(result, response_code, headers, body):
-	add_text($Node/OuputLabel, "result: " + str(result))
-	add_text($Node/OuputLabel, "response code: " + str(response_code))
-	add_text($Node/OuputLabel, "headers: ")
-	for i in headers:
-		add_text($Node/OuputLabel, i)
-	
-	img_data.clear()
-	img_data = parse_img_data_from_html(body)
-	if img_data != []:
-		$FileDialog.popup_centered()
-	
-
-func _on_DownloadButton_pressed():
-	var titleid = $Node/TitleIdInput.text.to_int()
-	var episode_from = $Node/EpisodeFrom.value
-	var episode_to= $Node/EpisodeTo.value
-	if episode_from == episode_to:
-		get_naver_comic()
-	else:
-		var message = "downloading multiple episode function is not ready yet!"
-		add_text($Node/OuputLabel, message)
-
-
-func _on_EpisodeFrom_value_changed(value):
-	if $Node/EpisodeFrom.value > $Node/EpisodeTo.value:
-		$Node/EpisodeTo.value = value
-	$Node/EpisodeTo.min_value = value
-
-
+# ***** 3 *****
 func _on_FileDialog_confirmed():
-	img_save_path = $FileDialog.current_dir + "test.jpeg"
-	download_img_to_local(img_data, $FileDialog.current_dir)
+	img_save_path = $FileDialog.current_dir
+	download_img_to_local()
 
 
-func _on_HTTPRequestImg_request_completed(result, response_code, headers, body):
-	add_text($Node/OuputLabel, "result: " + str(result))
-	add_text($Node/OuputLabel, "response code: " + str(response_code))
-	add_text($Node/OuputLabel, "headers: ")
-	for i in headers:
-		add_text($Node/OuputLabel, i)
-	
+# ***** 4 *****
+func download_img_to_local():
+	request_header = ["User-Agent: Mozilla/5.1 (Windows NT 10.1; Win64; x64) AppleWebKit/536.37 (KHTML, like Gecko) Chrome/91.1.4480.73 Safari/535.36",
+				 "Referer: " + create_naver_comic_url(get_titleid_val(), get_episode_to_val()) 
+				]
+	if $HTTPRequest.is_connected("request_completed", self, "_on_getWebtoonPage_request_completed"):
+		$HTTPRequest.disconnect("request_completed", self, "_on_getWebtoonPage_request_completed")
+	if not $HTTPRequest.is_connected("request_completed", self, "_on_downloadMultipleImgs_request_completed"): 
+		$HTTPRequest.connect("request_completed", self, "_on_downloadMultipleImgs_request_completed")
+	$HTTPRequest.request(img_urls.pop_back(), request_header)
+	print(img_urls.size())
+
+
+# ***** 5 *****
+# this fuction is requires global variable "img_urls"
+func _on_downloadMultipleImgs_request_completed(result, response_code, headers, body):
+	print_HTTPRequest_RRH_on_output_display(result, response_code, headers)
 	if result == HTTPRequest.RESULT_SUCCESS: 
 		if response_code != 403:
 			var image = Image.new()
@@ -145,10 +161,29 @@ func _on_HTTPRequestImg_request_completed(result, response_code, headers, body):
 			if error != OK:
 				printerr("Couldn't load the image.")
 			
-			add_text($Node/OuputLabel, "Saving image to  " + img_save_path)
-			image.save_png(img_save_path)
+			print_on_output_display("Saving image to  " + img_save_path + "/" + str(img_urls.size()) + ".jpeg")
+			image.save_png(img_save_path + "/" + str(len(img_urls)) + ".png")
 		else:
 			printerr("response_code:", response_code)
 	else:
 		printerr("Unexpected result from HTTPRequest...  result code:", result)
 	
+	print(img_urls.size())
+	if img_urls.size() > 0:
+		download_img_to_local()
+
+
+# prints http request result, response, and headers to output display -- for debugging
+func print_HTTPRequest_RRH_on_output_display(result, response_code, headers):
+	print_on_output_display("result: " + str(result))
+	print_on_output_display("response code: " + str(response_code))
+	print_on_output_display("headers: ")
+	for i in headers:
+		print_on_output_display(i)
+	print_on_output_display("##################\n\n")
+
+
+func _on_EpisodeFrom_value_changed(value):
+	if value > get_episode_to_val():
+		_episode_to_input.value = value
+	_episode_to_input.min_value = value
